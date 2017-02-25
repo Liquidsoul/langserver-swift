d24d8bc08612ed99b5c0fdec13e9991aed74c846
Ryan Lovelett <ryan@lovelett.me>
2017-02-25 08:47:12 -0500

    Depend on libSwiftPM

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
         .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 1, 4)),
         .Package(url: "https://github.com/thoughtbot/Argo.git", majorVersion: 4),
         .Package(url: "https://github.com/edwardaux/Ogra.git", majorVersion: 4),
-        .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 3)
+        .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 3),
+        .Package(url: "https://github.com/RLovelett/swift-package-manager.git", majorVersion: 3)
     ]
 )
 
