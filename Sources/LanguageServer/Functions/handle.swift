ee09b974954fdc2bf0e2621626c2e663c4de3eaa
Ryan Lovelett <ryan@lovelett.me>
2017-03-20 16:41:59 -0400

    Playing around

--- Sources/LanguageServer/Functions/handle.swift
+++ Sources/LanguageServer/Functions/handle.swift
@@ -12,73 +12,73 @@ import Foundation
 import LanguageServerProtocol
 import class PackageLoading.ManifestLoader
 
-var workspace: Workspace!
-var exitCode: Int32 = 1
-
-func handle(_ request: Request) -> Response {
-    do {
-        switch request.method {
-        case "initialize":
-            let parameters: InitializeParams = try request.parse()
-            let toolchain = try LanguageServerToolchain()
-            let manifestLoader = ManifestLoader(resources: toolchain)
-            Server(<#T##parameters: InitializeParams##InitializeParams#>)
-            workspace = Workspace(parameters)
-            let response = Response(to: request, is: InitializeResult(workspace.capabilities))
-            return response
-        case "workspace/didChangeWatchedFiles":
-            let parameters: DidChangeWatchedFilesParams = try request.parse()
-            workspace.receive(notification: parameters)
-            return Response(to: request, is: JSON.null)
-        case "textDocument/didChange":
-            let document: DidChangeTextDocumentParams = try request.parse()
-            try workspace.client(modified: document)
-            return Response(to: request, is: JSON.null)
-        case "textDocument/didClose":
-            let document: DidCloseTextDocumentParams = try request.parse()
-            workspace.client(closed: document)
-            return Response(to: request, is: JSON.null)
-        case "textDocument/didOpen":
-            let document: DidOpenTextDocumentParams = try request.parse()
-            workspace.client(opened: document)
-            return Response(to: request, is: JSON.null)
-//        case "textDocument/didSave":
-//            fatalError("\(request.method) is not implemented yet.")
-        case "textDocument/completion":
-            let parameters: TextDocumentPositionParams = try request.parse()
-            let items = try workspace.complete(forText: parameters)
-            let response = Response(to: request, is: items)
-            return response
-        case "textDocument/definition":
-            let parameters: TextDocumentPositionParams = try request.parse()
-            let location = try workspace.findDeclaration(forText: parameters)
-            let response = Response(to: request, is: location)
-            return response
-        case "textDocument/hover":
-            let parameters: TextDocumentPositionParams = try request.parse()
-            let hover = try workspace.cursor(forText: parameters)
-            let response = Response(to: request, is: hover)
-            return response
-//        case "textDocument/documentSymbol":
-//            fatalError("\(request.method) is not implemented yet.")
-//        case "textDocument/references":
-//            fatalError("\(request.method) is not implemented yet.")
-//        case "workspace/symbol":
-//            fatalError("\(request.method) is not implemented yet.")
-        case "shutdown":
-            workspace = nil
-            // The server should exit with `success` code 0 if the shutdown request has been received
-            // before; otherwise with `error` code 1.
-            exitCode = 0
-            return Response(to: request, is: JSON.null)
-        case "exit":
-            exit(exitCode)
-        default:
-            throw PredefinedError.methodNotFound
-        }
-    } catch let error as ServerError {
-        return Response(to: request, is: error)
-    } catch {
-        return Response(to: request, is: PredefinedError.invalidParams)
-    }
-}
+//var workspace: Workspace!
+//var exitCode: Int32 = 1
+//
+//func handle(_ request: Request) -> Response {
+//    do {
+//        switch request.method {
+//        case "initialize":
+//            let parameters: InitializeParams = try request.parse()
+//            let toolchain = try LanguageServerToolchain()
+//            let manifestLoader = ManifestLoader(resources: toolchain)
+////            Server(<#T##parameters: InitializeParams##InitializeParams#>)
+////            workspace = Workspace(parameters)
+//            let response = Response(to: request, is: InitializeResult(workspace.capabilities))
+//            return response
+//        case "workspace/didChangeWatchedFiles":
+//            let parameters: DidChangeWatchedFilesParams = try request.parse()
+//            workspace.receive(notification: parameters)
+//            return Response(to: request, is: JSON.null)
+//        case "textDocument/didChange":
+//            let document: DidChangeTextDocumentParams = try request.parse()
+//            try workspace.client(modified: document)
+//            return Response(to: request, is: JSON.null)
+//        case "textDocument/didClose":
+//            let document: DidCloseTextDocumentParams = try request.parse()
+//            workspace.client(closed: document)
+//            return Response(to: request, is: JSON.null)
+//        case "textDocument/didOpen":
+//            let document: DidOpenTextDocumentParams = try request.parse()
+//            workspace.client(opened: document)
+//            return Response(to: request, is: JSON.null)
+////        case "textDocument/didSave":
+////            fatalError("\(request.method) is not implemented yet.")
+//        case "textDocument/completion":
+//            let parameters: TextDocumentPositionParams = try request.parse()
+//            let items = try workspace.complete(forText: parameters)
+//            let response = Response(to: request, is: items)
+//            return response
+//        case "textDocument/definition":
+//            let parameters: TextDocumentPositionParams = try request.parse()
+//            let location = try workspace.findDeclaration(forText: parameters)
+//            let response = Response(to: request, is: location)
+//            return response
+//        case "textDocument/hover":
+//            let parameters: TextDocumentPositionParams = try request.parse()
+//            let hover = try workspace.cursor(forText: parameters)
+//            let response = Response(to: request, is: hover)
+//            return response
+////        case "textDocument/documentSymbol":
+////            fatalError("\(request.method) is not implemented yet.")
+////        case "textDocument/references":
+////            fatalError("\(request.method) is not implemented yet.")
+////        case "workspace/symbol":
+////            fatalError("\(request.method) is not implemented yet.")
+//        case "shutdown":
+//            workspace = nil
+//            // The server should exit with `success` code 0 if the shutdown request has been received
+//            // before; otherwise with `error` code 1.
+//            exitCode = 0
+//            return Response(to: request, is: JSON.null)
+//        case "exit":
+//            exit(exitCode)
+//        default:
+//            throw PredefinedError.methodNotFound
+//        }
+//    } catch let error as ServerError {
+//        return Response(to: request, is: error)
+//    } catch {
+//        return Response(to: request, is: PredefinedError.invalidParams)
+//    }
+//}
--- Sources/LanguageServer/main.swift
+++ Sources/LanguageServer/main.swift
@@ -1,56 +1,98 @@
-import struct Basic.AbsolutePath
-import Workspace
-import class PackageLoading.ManifestLoader
-import func Build.describe
-import struct Utility.BuildFlags
+import BaseProtocol
+import Dispatch
+import Foundation
+import LanguageServerProtocol
 
-class ToolWorkspaceDelegate: WorkspaceDelegate {
-    func fetchingMissingRepositories(_ urls: Set<String>) {
-    }
+private let header: [String : String] = [
+    "Content-Type": "application/vscode-jsonrpc; charset=utf8"
+]
 
-    func fetching(repository: String) {
-        print("Fetching \(repository)")
-    }
+enum Result<R> {
+    case Success(R)
+    case Failure(Error)
+}
 
-    func cloning(repository: String) {
-        print("Cloning \(repository)")
-    }
+typealias Async<A, B> = (_ a: A, _ handler: @escaping (Result<B>) -> Void) -> Void
 
-    func checkingOut(repository: String, at reference: String) {
-        // FIXME: This is temporary output similar to old one, we will need to figure
-        // out better reporting text.
-        print("Resolving \(repository) at \(reference)")
-    }
+infix operator •
 
-    func removing(repository: String) {
-        print("Removing \(repository)")
-    }
-
-    func warning(message: String) {
-        print("warning: " + message)
+func •<A, B, C>(f: @escaping Async<A, B>, g: @escaping Async<B, C>) -> Async<A, C> {
+    return { a, handler in
+        f(a, { result in
+            switch result {
+            case .Success(let b): g(b, handler)
+            case .Failure(let e): handler(.Failure(e))
+            }
+        })
     }
 }
 
-let toolchain = try! LanguageServerToolchain()
+func f(input: FileHandle, h: @escaping (Result<Data>) -> ()) {
+    let main = OperationQueue.main
+    let dataAvailable = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: input, queue: main) { (notification) -> () in
+//        sleep(2000)
+//        NotificationCenter.default.removeObserver(dataAvailable)
+        h(.Success(input.availableData))
+    }
 
-let path = AbsolutePath("/Users/ryan/Source/langserver-swift/Fixtures/ValidLayouts/Simple")
-let buildPath = path.appending(component: ".build")
-let edit = path.appending(component: "Packages")
-let pins = path.appending(component: "Package.pins")
-
-let manifestLoader = ManifestLoader(resources: toolchain)
-let delegate = ToolWorkspaceDelegate()
-
-let ws = try! Workspace(dataPath: buildPath, editablesPath: edit, pinsFile: pins, manifestLoader: manifestLoader, delegate: delegate)
-
-let buildFlags = BuildFlags(xcc: nil, xswiftc: nil, xlinker: nil)
-
-do {
-  ws.registerPackage(at: path)
-  let pg = try ws.loadPackageGraph()
-  dump(pg)
-  let yaml = try describe(buildPath, .debug, pg, flags: buildFlags, toolchain: toolchain)
-  dump(yaml)
-} catch let error {
-    print(error)
+    input.waitForDataInBackgroundAndNotify()
 }
+
+func g(buffer: RequestBuffer? = nil) -> (Data, (Result<RequestBuffer>) -> ()) -> () {
+    return { (i: Data, handler: (Result<RequestBuffer>) -> ()) in
+        switch buffer {
+        case .some(let b):
+            b.append(i)
+            handler(Result.Success(b))
+        case .none:
+            handler(Result.Success(RequestBuffer(i)))
+        }
+    }
+}
+
+let chained = f • g()
+
+chained(FileHandle.standardInput) { result in
+    switch result {
+    case .Success(let request):
+        dump(request)
+//        exit(0)
+    case .Failure(let e):
+        NSLog(e.localizedDescription)
+    }
+}
+
+// When new data is available
+//var dataAvailable : NSObjectProtocol!
+//dataAvailable = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: stdin, queue: main) { (notification) -> Void in
+//    let buffer = stdin.availableData
+//
+//    guard !buffer.isEmpty else {
+//        return stdin.waitForDataInBackgroundAndNotify()
+//    }
+//
+////    requests.append(buffer)
+////
+////    for requestBuffer in requests {
+////        do {
+////            let request = try Request(requestBuffer)
+////            let response = handle(request)
+////            /// If the request id is null then it is a notification and not a request
+////            switch request {
+////            case .request(_, _, _):
+////                let toSend = response.data(header)
+////                FileHandle.standardOutput.write(toSend)
+////            default: ()
+////            }
+////        } catch let error as PredefinedError {
+////            fatalError(error.description)
+////        } catch {
+////            fatalError("TODO: Better error handeling. \(error)")
+////        }
+////    }
+////
+////    return stdin.waitForDataInBackgroundAndNotify()
+//}
+
+// Launch the task
+RunLoop.main.run()
