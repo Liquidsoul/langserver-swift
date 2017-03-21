a1a9da6b3e69fa9415f6755fcd5b419273176e54
Ryan Lovelett <ryan@lovelett.me>
2017-03-20 22:29:25 -0400

    More React

--- Sources/LanguageServer/main.swift
+++ Sources/LanguageServer/main.swift
@@ -1,12 +1,11 @@
 import BaseProtocol
-import Dispatch
 import Foundation
 import LanguageServerProtocol
 import enum Result.NoError
 import ReactiveSwift
 
-let signal = Signal<Data, NoError> { observer in
-    let no = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: FileHandle.standardInput, queue: OperationQueue.main) { _ in
+let stdin = Signal<Data, NoError> { observer in
+    let no = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: FileHandle.standardInput, queue: nil) { _ in
         let data = FileHandle.standardInput.availableData
         if data.isEmpty {
             observer.sendCompleted()
@@ -21,48 +20,29 @@ let signal = Signal<Data, NoError> { observer in
     }
 }
 
-signal.observeCompleted {
+stdin.scan(RequestBuffer()) { (buffer, Data) -> RequestBuffer in
+    buffer.append(Data)
+    return buffer
+}
+.flatMap(.merge) { b -> SignalProducer<Data, NoError> in
+    return SignalProducer(AnySequence { b })
+}
+.filter({ !$0.isEmpty })
+.attemptMap({ try Request($0) })
+.map(handle())
+.observeResult { result in
+    switch result {
+    case .success(let r):
+        dump(r)
+    case .failure(let e):
+        dump(e)
+    }
+}
+
+stdin.observeCompleted {
     exit(0)
 }
 
-signal.observeValues({
-    dump($0)
-})
-
-FileHandle.standardInput.waitForDataInBackgroundAndNotify()
-
-
-// When new data is available
-//var dataAvailable : NSObjectProtocol!
-//dataAvailable = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: stdin, queue: main) { (notification) -> Void in
-//    let buffer = stdin.availableData
-//
-//    guard !buffer.isEmpty else {
-//        return stdin.waitForDataInBackgroundAndNotify()
-//    }
-//
-////    requests.append(buffer)
-////
-////    for requestBuffer in requests {
-////        do {
-////            let request = try Request(requestBuffer)
-////            let response = handle(request)
-////            /// If the request id is null then it is a notification and not a request
-////            switch request {
-////            case .request(_, _, _):
-////                let toSend = response.data(header)
-////                FileHandle.standardOutput.write(toSend)
-////            default: ()
-////            }
-////        } catch let error as PredefinedError {
-////            fatalError(error.description)
-////        } catch {
-////            fatalError("TODO: Better error handeling. \(error)")
-////        }
-////    }
-////
-////    return stdin.waitForDataInBackgroundAndNotify()
-//}
-
 // Launch the task
+FileHandle.standardInput.waitForDataInBackgroundAndNotify()
 RunLoop.main.run()
